6d407ee273abcb3a345a85c91358b664b6b828cd
Dasko <dasko2200@gmail.com>
2025-02-16 12:35:54 -0500

    Commit time_to_file.py

--- time_to_file.py
+++ time_to_file.py
@@ -1,2 +1,15 @@
 # Finds the time and date and then appends it to a file!
 
+from datetime import datetime
+
+now = datetime.now()
+
+# Create the formatted string for the date!
+formatted_date = now.strftime("%m-%d-%Y")
+
+# Print the time to a file!
+with open("file.txt", "w") as f:
+    f.write("The date is: " + formatted_date)
+
+f = open("file.txt", "r")
+print(f.read())
