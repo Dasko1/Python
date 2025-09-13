c9bd52e1a4072e93861128d50b8b03a19074e433
Dasko <dasko2200@gmail.com>
2025-09-13 19:38:30 -0400

    Converted AddBills.py to Python3!

--- AddBills.py
+++ AddBills.py
@@ -1,12 +1,11 @@
-#AddBills.py: this keeps a running amount.
-#Idea: Have the script give you a running amount every 5 entries.
+# AddBills.py: this keeps a running amount.
+# Idea: Have the script give you a running amount every 5 entries.
 
 x = 1
-sum = 0
-runsum = 0
+sum1 = 0
 
 while x != 0:
-    x = float(raw_input("Enter an amount:"))
-    sum = sum + x
+    x = float(input("Enter an amount:"))
+    sum1 = sum1 + x
 
-print "\nThe total is: ",sum
+print("\nThe total is: " + str(sum1))
